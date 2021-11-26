6393de688a7b50588edccbf5c18a04c2a020b3a1
DmitryReztsov <rez_dima@mail.ru>
2021-11-26 20:15:03 +0300

    add resize event

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
-const mainWindowWidth = document.documentElement.clientWidth;
-const mainWindowHeight = document.documentElement.clientHeight;
+let mainWindowWidth = document.documentElement.clientWidth;
+let mainWindowHeight = document.documentElement.clientHeight;
 
 // Делаем трапецию для желтого фона
 function calculateLeft() {
@@ -7,8 +7,7 @@ function calculateLeft() {
 	const left = document.querySelector(".main-screen__left");
 	// задаем размеры рамок элемента, чтобы получилась трапеция 
 	// делаем это через js, так как в border не получается указать % величину от блока,
-	// тогда берем от размера окна (минус этого подхода- нужно обновлять страницу при просмотре DevTools,
-	// если меняем величину окна, иначе все ломается)
+	
 	left.style.borderBottom = `${mainWindowHeight}px solid #f4e770`;
 	left.style.borderLeft = `${mainWindowWidth * 0.8}px solid transparent`;
 }
@@ -16,10 +15,19 @@ function calculateLeft() {
 // запускаем при загрузке страницы
 calculateLeft()
 
+// Если меняется размер окна, используем событие resize для правильного рендеринга фона
+window.onresize = () => {
+	
+	mainWindowWidth = document.documentElement.clientWidth;
+	mainWindowHeight = document.documentElement.clientHeight;
+	
+	calculateLeft()
+}
+
 // Вешаем обработчик на кнопку
 document.querySelector(".main-screen__button").onclick = createWindow;
 
-// создаем новое окно с заготовленной страницей
+// создаем функцию для открытия нового окна с заготовленной страницей
 function createWindow() {
 
 	// указываем параметры нового окна с учетом размеров создающего окна
@@ -28,3 +36,4 @@ function createWindow() {
 	left=${mainWindowWidth / 2 - mainWindowWidth * 0.6 / 2},top=${mainWindowHeight / 2 - mainWindowHeight * 0.8 / 2}`
 	let newWindow = window.open("./secondpage.html", "storePage", params);
 }
+
